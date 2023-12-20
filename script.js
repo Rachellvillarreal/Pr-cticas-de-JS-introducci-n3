0976421743a0dd4fd4683f1006ae52ad32305f42
RachellVillarreal <teranrachellkatiuska@gmail.com>
2023-12-20 16:03:14 +0100

    actualizo repositorio subiendo el ejemplo numero2

--- script.js
+++ script.js
@@ -5,3 +5,13 @@ console.log("aqui amamos el sushi y el sashimi")
 const sumarNumeros = (numero1, numero2) => numero1 + numero2;
 const resultado = sumarNumeros(5, 7)
 console.log(resultado);
+
+//ejemplo 2//
+
+function sumarnumeros( nuero1, numero2) {
+    return nuero1 + numero2;
+}
+//ejemplo del uso de la funcion//
+var resultado1= sumarnumeros(5, 7);
+console.log(resultado1);
+
