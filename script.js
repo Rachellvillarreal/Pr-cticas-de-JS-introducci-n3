f0e6c4c3dbb7936092630175b96751a7c4dea7fa
RachellVillarreal <teranrachellkatiuska@gmail.com>
2023-12-20 15:42:47 +0100

    actualizo cambios realizado un ejemplo de suma en JS

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 console.log("aqui amamos el sushi y el sashimi")
+
+//ejeplo//
+
+const sumarNumeros = (numero1, numero2) => numero1 + numero2;
+const resultado = sumarNumeros(5, 7)
+console.log(resultado);
